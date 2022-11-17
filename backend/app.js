c7be4ac5da344f16d509819266f7d18c57d14f6c
LassiVaisanen <t1vala02@students.oamk.fi>
2022-11-17 17:18:46 +0200

    Added basic login functionality to backend. CRUD operations for card.

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require("dotenv").config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+var cardRouter = require('./routes/card');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -16,5 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/card', cardRouter);
+app.use('/login', loginRouter);
 
 module.exports = app;
