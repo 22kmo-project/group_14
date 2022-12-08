a6aa8f638e9f9c1062249270cfcb0c011d55668a
LassiVaisanen <t1vala02@students.oamk.fi>
2022-12-08 17:54:36 +0200

    Add missing backend features for login and card_model

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -17,19 +17,35 @@ router.post('/',
           }
           else{
             if (dbResult.length > 0) {
-              bcrypt.compare(pin,dbResult[0].pin, function(err,compareResult) {
-                if(compareResult) {
-                  console.log("Success");
-                  const token = generateAccessToken({ username: id_card });
-                  response.send(token);
+              card.checkIfCardLocked(id_card, function(dbError2, dbResult2) {
+                if(dbError2) {
+                  response.json(dbError2);
+                } else {
+                  console.log(dbResult2[0][0]["is_locked"]);
+                  bcrypt.compare(pin,dbResult[0].pin, function(err,compareResult) {
+                    if(compareResult && dbResult2[0][0]["is_locked"] == 0) { // is_locked == 0 means card is not locked
+                      console.log("Success");
+                      const token = generateAccessToken({ username: id_card });
+                      response.send(token);
+                    } 
+                    else if(compareResult && dbResult2[0][0]["is_locked"] == 1) { // is_locked == 1 means card is locked
+                      console.log("Card is locked");
+                      response.send({ isLocked: 1 });
+                    }
+                    else {
+                        console.log("Wrong password");
+                        console.log(dbResult[0].pin);
+                        card.updateWrongPinCounter(id_card, function(dbError, dbResult) {
+                          if(dbError) {
+                            response.json(dbError);
+                          }
+                        });
+                        response.send(false);
+                    }			
+                  }
+                  );
                 }
-                else {
-                    console.log("Wrong password");
-                    console.log(dbResult[0].pin);
-                    response.send(false);
-                }			
-              }
-              );
+              });
             }
             else{
               console.log("User does not exist");
